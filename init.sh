7cf76165d0df4922e8b83330af9d4d222841ab22
Jeffrey Shen <jeffrey.shen@displayr.com>
2017-11-21 08:28:36 +1100

    Create init.sh

---
+++ init.sh
@@ -0,0 +1,31 @@
+sudo apt install zsh vim git
+# oh my zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+sed -e s/ZSH_THEME=.*/ZSH_THEME="agnoster"/g
+
+
+# change to vim
+sudo update-alternatives --config editor
+cp generate.vim ~/.vimrc
+
+sudo apt-get install python-dev python-pip python3-dev python3-pip
+# neovim
+#sudo add-apt-repository ppa:neovim-ppa/stable
+#sudo apt-get update
+#sudo apt-get install neovim
+#sudo update-alternatives --install /usr/bin/vi vi /usr/bin/nvim 60
+#sudo update-alternatives --config vi
+#sudo update-alternatives --install /usr/bin/vim vim /usr/bin/nvim 60
+#sudo update-alternatives --config vim
+#sudo update-alternatives --install /usr/bin/editor editor /usr/bin/nvim 60
+#sudo update-alternatives --config editor
+#cp neogenerate.vim ~/.config/nvim/init.vim
+
+
+
+# R packages
+sudo apt-get install protobuf-compiler libprotobuf-dev libprotoc-dev python-protobuf librsvg2-dev libapparmor-dev apparmor-utils libhiredis-dev libev-dev redis-tools build-essential tcl
+R -e install.packages(c('RProtoBuf''RCurl','rjson
+echo "github_token:"
+read AUTH_TOKEN
+R -e devtools::install_github(auth_token=$AUTH_TOKEN )
